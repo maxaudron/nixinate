bcf847f1c8cd42be10e41a0daec1684b99fcbab0
Max Audron <audron@cocaine.farm>
2023-10-10 15:26:08 +0200

    test

--- nixinate/make-deploy-script.nix
+++ nixinate/make-deploy-script.nix
@@ -11,8 +11,7 @@ let
 
   n = flake.nixosConfigurations.${machine}.config.deploy;
   hermetic = n.hermetic;
-  user = sshUser;
-  conn = if user then "${n.sshUser}@${n.host}" else "${n.host}";
+  conn = if (n.sshUser != null) then "${n.sshUser}@${n.host}" else "${n.host}";
   where = n.buildOn or "remote";
   remote = if where == "remote" then true else if where == "local" then false else abort "_module.args.nixinate.buildOn is not set to a valid value of 'local' or 'remote'";
   substituteOnTarget = n.substituteOnTarget or false;
